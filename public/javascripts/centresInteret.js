4e05e0f74d7c008e6a0035703d6b851df442caf9
Florine <florine.robitaille@orange.fr>
2019-06-15 17:12:35 +0200

    Pop-up Centre d'interet + js + style

--- public/javascripts/centresInteret.js
+++ public/javascripts/centresInteret.js
@@ -1,8 +1,34 @@
-//#grille .icon
-$( "#grille .icon" ).on('click',function(){ 
-    //"Dans le bloc #grille n'importe quel .icon dès que je clic je fais :"
-    console.log($(this).attr('id'));
-    $(this).toggleClass('active');
-    //$(this).hide(); 
+$(function () {
+    $("#grille").randomize("#grille > div");
+    var count = $("#grille .icon").length;
+    console.log(count);
+    for (var i = 0; i < count/2; i++) {
+        $("#grille .icon")[i].remove();
+    }
 });
 
+(function ($) {
+
+    $.fn.randomize = function (childElem) {
+        return this.each(function () {
+            var $this = $(this);
+            var elems = $this.children(childElem);
+
+            elems.sort(function () {
+                return (Math.round(Math.random()) - 0.5);
+            });
+
+            $this.remove(childElem);
+
+            for (var i = 0; i < elems.length; i++)
+                $this.append(elems[i]);
+
+        });
+    }
+})(jQuery);
+
+//#grille .icon
+$("#grille .icon").on('click', function () {
+    console.log($(this).attr('id'));
+    $(this).parent().toggleClass('active');
+});
